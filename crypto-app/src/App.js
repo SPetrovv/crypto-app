7516176f82df1447f6b07442c5916a921e55d739
Stoyan <s.i.petrovvvv@gmail.com>
2024-02-04 01:40:09 +0200

    adding transactions and balances

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,26 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'; // Import your CSS file if you have one
+import Web3 from 'web3';
+import axios from 'axios';
 
 function App() {
   const [connected, setConnected] = useState(false);
   const [metamaskAddress, setMetamaskAddress] = useState('');
+  const [balance, setBalance] = useState(0);
+  const [tokenBalances, setTokenBalances] = useState([]);
+  const [latestTransactions, setLatestTransactions] = useState([]);
+  const [web3, setWeb3] = useState(null);
 
   const connectToMetamask = async () => {
     try {
-      // Check if Metamask is installed and isMetaMask is true
-      if (window.ethereum && window.ethereum.isMetaMask) {
-        // Request account access if needed using ethereum.enable()
-        const accounts = await window.ethereum.enable();
-        const address = accounts[0];
-        console.log('Connected to Metamask! Address:', address);
-        setMetamaskAddress(address);
+      if (window.ethereum) {
+        await window.ethereum.enable();
+        const web3Instance = new Web3(window.ethereum);
+        const accounts = await web3Instance.eth.getAccounts();
+        setMetamaskAddress(accounts[0]);
+
+        // Get the balance
+        const weiBalance = await web3Instance.eth.getBalance(accounts[0]);
+        const etherBalance = web3Instance.utils.fromWei(weiBalance, 'ether');
+        setBalance(etherBalance);
+
+        // Get token balances
+        const tokenBalancesData = await getTokenBalances(web3Instance, accounts[0]);
+        setTokenBalances(tokenBalancesData);
+
+        // Get latest transactions
+        getLatestTransactions(web3Instance, accounts[0]);
+
+        setWeb3(web3Instance);
         setConnected(true);
       } else {
-        console.error('Metamask not found.');
+        console.error('Metamask not detected!');
       }
     } catch (error) {
       console.error('Error connecting to Metamask:', error);
-      setConnected(false);
+    }
+  };
+
+  const getTokenBalances = async (web3, address) => {
+    try {
+      // Fetch token balances from Etherscan API
+      const response = await axios.get(
+        `https://api.etherscan.io/api?module=account&action=tokenlist&address=${address}&tag=latest&apikey=IZF6N6FF264DP4IY16IZ8V75ARFJRZFEIS`
+      );
+
+      if (response.data.status === '1') {
+        const tokens = response.data.result;
+        const tokenBalancesData = await Promise.all(tokens.map(async (token) => {
+          const tokenBalanceResponse = await axios.get(
+            `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${token.contractAddress}&address=${address}&tag=latest&apikey=IZF6N6FF264DP4IY16IZ8V75ARFJRZFEIS`
+          );
+          return {
+            name: token.name,
+            symbol: token.symbol,
+            balance: tokenBalanceResponse.data.result,
+          };
+        }));
+        return tokenBalancesData;
+      } else {
+        console.error('Error fetching token balances:', response.data.message);
+        return [];
+      }
+    } catch (error) {
+      console.error('Error fetching token balances:', error);
+      return [];
+    }
+  };
+
+  const getLatestTransactions = async (web3, address) => {
+    try {
+      // Fetch latest transactions from Etherscan API
+      const response = await axios.get(
+        `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&sort=desc&apikey=IZF6N6FF264DP4IY16IZ8V75ARFJRZFEIS`
+      );
+  
+      if (response.data.status === '1') {
+        const transactions = response.data.result;
+        setLatestTransactions(transactions);
+      } else {
+        console.error('Error fetching latest transactions:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching latest transactions:', error);
     }
   };
 
@@ -44,44 +109,97 @@ function App() {
     }
   }, []);
 
+
+  const windowStyle = {
+    width: '70vw', // 50% of the viewport width
+    height: '20vw', // Adjust the height to maintain the square aspect ratio (half of the width)
+    backgroundColor: 'white', // Set to black
+    borderRadius: '10px', // Adjust the radius as needed
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    padding: '20px', // Add padding for better spacing
+    textAlign: 'center', // Center-align text
+  };
+
   return (
-    <div style={{ backgroundColor: 'black', height: '100vh', color: 'white', position: 'relative' }}>
+    <div className="app-container">
       {connected ? (
-        <div style={{ position: 'absolute', top: 10, right: 10, display: 'flex', alignItems: 'center' }}>
-          <p style={{ padding: '10px', borderRadius: '8px', backgroundColor: 'green', marginRight: '10px' }}>
-            Account: {`${metamaskAddress.slice(0, 4)}...${metamaskAddress.slice(-4)}`}
-          </p>
-          <button
-            style={{
-              padding: '10px 20px',
-              borderRadius: '8px',
-              fontSize: '16px',
-              backgroundColor: 'red',
-              color: 'white',
-              border: 'none',
-              marginLeft: '10px', // Add marginLeft to align the Disconnect button
-            }}
-            onClick={disconnectFromMetamask}
-          >
-            Disconnect
-          </button>
+        <div className="connected-container">
+          <div className="metamask-info">
+            <p>
+              Account: {`${metamaskAddress.slice(0, 4)}...${metamaskAddress.slice(-4)}`}
+            </p>
+          </div>
+          <div className="disconnect-container">
+            <button
+              className="disconnect-button"
+              onClick={disconnectFromMetamask}
+            >
+              Disconnect
+            </button>
+          </div>
         </div>
       ) : (
-        <button
-          style={{
-            position: 'absolute',
-            top: 10,
-            right: 10,
-            padding: '10px 20px',
-            borderRadius: '8px',
-            fontSize: '16px',
-          }}
-          onClick={connectToMetamask}
-        >
-          Connect
-        </button>
+        <div className="connect-container">
+          <button
+            className="connect-button"
+            onClick={connectToMetamask}
+          >
+            Connect
+          </button>
+        </div>
       )}
-      {/* Your content goes here */}
+      {/* <div className="window">
+  
+      </div> */}
+      <div style={windowStyle}>
+      <div className="balances-container">
+        <div className="token-balances">
+          <h2>Token Balances</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Token</th>
+                <th>Symbol</th>
+                <th>Balance</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>ETH</td>
+                <td>ETH</td>
+                <td>{balance} ETH</td>
+              </tr>
+              {tokenBalances.map((token, index) => (
+                <tr key={index}>
+                  <td>{token.name}</td>
+                  <td>{token.symbol}</td>
+                  <td>{token.balance}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="latest-transactions-container">
+          <h2 className="latest-transactions-header">Latest Transactions</h2>
+          <div className="latest-transactions-list">
+            <ul>
+              {latestTransactions.map((transaction, index) => (
+                <li key={index}>
+                  <p>Hash: {transaction.hash}</p>
+                  <p>From: {transaction.from}</p>
+                  <p>To: {transaction.to}</p>
+                  <p>Value: {web3 ? web3.utils.fromWei(transaction.value, 'ether') : ''} ETH</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
